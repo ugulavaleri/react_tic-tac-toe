edcc6b18129d84dfd6c45ab7be5313b51a462d71
ugulavaleri <leri.ugulava1@gmail.com>
2023-05-05 21:56:44 +0400

    changed title

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+import Main from "./components/TicTacToe";
+
+// Pascal Casing  !!!
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // const user = {
+    //     name: "mon",
+    //     surname: "key",
+    //     imageURL:
+    //         "https://media.npr.org/assets/img/2014/08/07/monkey-selfie_custom-7117031c832fc3607ee5b26b9d5b03d10a1deaca-s600-c85.webp",
+    // };
+
+    return (
+        <div>
+            <Main />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/TicTacToe.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./TicTacToe.css";
+
+const Cube = ({ value, onClickEvent }) => {
+    return (
+        <td onClick={onClickEvent} className="cube">
+            {value}
+        </td>
+    );
+};
+
+const Header = () => {
+    const [block, setBlock] = useState(Array(9).fill(null));
+
+    const changeValue = (e) => {
+        const clone = [...block];
+        clone[e] = "X";
+        setBlock(clone);
+    };
+
+    return (
+        <table>
+            <tr>
+                <Cube value={block[0]} onClickEvent={() => changeValue(0)} />
+                <Cube value={block[1]} onClickEvent={() => changeValue(1)} />
+                <Cube value={block[2]} onClickEvent={() => changeValue(2)} />
+            </tr>
+            <tr>
+                <Cube value={block[3]} onClickEvent={() => changeValue(3)} />
+                <Cube value={block[4]} onClickEvent={() => changeValue(4)} />
+                <Cube value={block[5]} onClickEvent={() => changeValue(5)} />
+            </tr>
+            <tr>
+                <Cube value={block[6]} onClickEvent={() => changeValue(6)} />
+                <Cube value={block[7]} onClickEvent={() => changeValue(7)} />
+                <Cube value={block[8]} onClickEvent={() => changeValue(8)} />
+            </tr>
+        </table>
+    );
+};
+
+export default Header;
